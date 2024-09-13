44345b2a3e30e00ad03504ad50125a876468e0d8
Andrew Crews <>
2024-09-13 04:10:05 -0400

    Updated Portfolio

---
+++ app.js
@@ -0,0 +1,7 @@
+$('#github').click(function(){
+    window.open('https://github.com/AndrewCrews3');
+ })
+
+ $('#linkedin').click(function(){
+    window.open('https://www.linkedin.com/in/andrew-crews-23ba92210/');
+ })
