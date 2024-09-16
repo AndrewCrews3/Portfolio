cf222353c7c8a4003e7cddc62fbc3c880b48349c
Andrew Crews <>
2024-09-15 22:44:24 -0400

    made changes and i might mess this up bad soon enough

--- app.js
+++ app.js
@@ -5,3 +5,27 @@ $('#github').click(function(){
  $('#linkedin').click(function(){
     window.open('https://www.linkedin.com/in/andrew-crews-23ba92210/');
  })
+
+$('.aboutme').click(function() {
+   $('html, body').animate({
+       scrollTop: $(".aboutme-container").offset().top
+   }, 1000);
+});
+
+$('.skill').click(function() {
+   $('html, body').animate({
+       scrollTop: $(".skills").offset().top - 120
+   }, 1000);
+});
+
+$('.education').click(function() {
+   $('html, body').animate({
+       scrollTop: $(".skills-logo-container").offset().top
+   }, 1000);
+});
+
+$('.connect').click(function() {
+   $('html, body').animate({
+       scrollTop: $(".skills-logo-container").offset().top
+   }, 1000);
+});
